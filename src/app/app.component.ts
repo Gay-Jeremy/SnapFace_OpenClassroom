f8444913dfb13dca6e285bec8e1d223c0b8cd4ff
Jeremy.Dev <jeremy.gay.dev@gmail.com>
2026-03-09 14:09:15 +0100

    ajout composant face-snap

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component, signal} from '@angular/core';
+import { FaceSnap } from './face-snap/face-snap';
+
+
+@Component({
+  selector: 'app-root',
+  imports: [FaceSnap],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class App {
+  
+}
