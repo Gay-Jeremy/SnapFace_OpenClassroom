8771d066de151da365c2d7e7bcd500905ecf473d
Jeremy.Dev <jeremy.gay.dev@gmail.com>
2026-03-10 11:04:06 +0100

    ajout service

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,15 +1,17 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FaceSnap } from '../models/face-snaps';
-import { NgClass, NgStyle } from '@angular/common';
+import { CurrencyPipe, DatePipe, DecimalPipe, NgClass, NgStyle, PercentPipe, UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-face-snap',
-  imports: [NgStyle, NgClass],
+  imports: [NgStyle, NgClass, UpperCasePipe, DatePipe, DecimalPipe,PercentPipe],
   templateUrl: './face-snap.component.html',
   styleUrl: './face-snap.component.scss',
 })
 
 export class FaceSnapComponent implements OnInit {
+
+  
 
   @Input() faceSnap!: FaceSnap
 
